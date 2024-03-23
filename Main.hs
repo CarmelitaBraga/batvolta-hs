05b38f329215b799c61c793d9d4ffc309ee21bac
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-03-23 19:19:19 -0300

    feat: Main para teste da criação de uma Carona

--- Main.hs
+++ Main.hs
@@ -1,5 +1,23 @@
 module Main where
+
 import Data.Csv
+import Data.Time
+import Data.Time.Calendar 
+import Src.Schema.CaronaSchema as CARONA
+import Data.Time.LocalTime
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    -- Obtém a data e hora atuais
+    currentTime <- getCurrentTime
+
+    -- Converte a UTCTime para o tipo Day (extrai apenas a data)
+    let today = utctDay currentTime
+
+        -- Obtém a hora atual no fuso horário local
+    zonedTime <- getZonedTime
+
+    -- Extrai o horário atual do ZonedTime
+    let timeOfDay = localTimeOfDay (zonedTimeToLocalTime zonedTime)
+
+    CARONA.criarCarona timeOfDay today "Casa de Luana" "Casa de Everton" "André" 19.99
