2aa9b9329381651860e319f87cefd7fd60ed7793
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-03-23 19:17:52 -0300

    feat: comentado para teste

--- src/Schema/Database.hs
+++ src/Schema/Database.hs
@@ -1,21 +1,25 @@
-module Database where
-    import Data.Csv (FromNamedRecord, ToNamedRecord, decodeByName, encodeByName)
-    import Data.ByteString.Lazy (ByteString)
-    import Data.Vector (Vector)
+-- module Src.Schema.Database (
+--     writeCSV,
+--     readCSV
+-- ) where
 
--- Define your CSV data type here
-data CSVData = CSVData {
-    -- ...
-} deriving (Show, Eq, FromNamedRecord, ToNamedRecord)
+-- import Data.Csv (FromNamedRecord, ToNamedRecord, decodeByName, encodeByName)
+-- import Data.ByteString.Lazy (ByteString)
+-- import Data.Vector (Vector)
 
--- Read a CSV file into a list of CSVData
-readCSV :: FilePath -> IO (Either String (Vector CSVData))
-readCSV filePath = do
-    csvData <- readFile filePath
-    return $ decodeByName csvData
+-- -- Define your CSV data type here
+-- newtype CSVData = CSVData {
+--     text :: String
+-- } deriving (Show, Eq)
 
--- Write a list of CSVData to a CSV file
-writeCSV :: FilePath -> Vector CSVData -> IO ()
-writeCSV filePath csvData = do
-    let csvEncoded = encodeByName csvData
-    writeFile filePath csvEncoded
+-- -- Read a CSV file into a list of CSVData
+-- readCSV :: FilePath -> IO (Either String (Vector CSVData))
+-- readCSV filePath = do
+--     csvData <- readFile filePath
+--     return $ decodeByName csvData
+
+-- -- Write a list of CSVData to a CSV file
+-- writeCSV :: FilePath -> Vector CSVData -> IO ()
+-- writeCSV filePath csvData = do
+--     let csvEncoded = encodeByName csvData
+--     writeFile filePath csvEncoded
