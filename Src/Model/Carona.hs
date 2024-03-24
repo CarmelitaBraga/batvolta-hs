d91d0cf0de6d33ab6b83831db63b4e2d9acdf8c1
CarmelitaBraga <carmelitabraga8@gmail.com>
2024-03-24 18:30:33 -0300

    feat: create Carona model with implementations to strToCarona and caronaToStr

---
+++ Src/Model/Carona.hs
@@ -0,0 +1,76 @@
+module Src.Model.Carona where
+
+import Data.Time.LocalTime (TimeOfDay)
+import Data.Time.Calendar (Day)
+import Data.List.Split (splitOn)
+import Debug.Trace (traceShow)
+import Data.List (intercalate)
+import Numeric (showFFloat)
+
+data Carona = Carona
+  { cid :: Int,
+    hora :: TimeOfDay,
+    date :: Day,
+    origem :: String,
+    destino :: String,
+    motorista :: String,
+    passageiros :: [String],
+    valor :: Double,
+    avaliacaoMotorista :: Int,
+    avaliacoesPassageiros :: [Int]
+  }
+  deriving (Show, Eq)
+
+-- Function to convert a Carona to a string
+caronaToStr :: Carona -> String
+caronaToStr (Carona c h d o dest m ps v am aps) =
+  show c ++ "," ++
+  show h ++ "," ++
+  show d ++ "," ++
+  o ++ "," ++
+  dest ++ "," ++
+  m ++ "," ++
+  intercalate ";" ps ++ "," ++
+  formatDecimal v ++ "," ++
+  show am ++ "," ++
+  intercalate ";" (map show aps)
+  where
+    formatDecimal :: Double -> String
+    formatDecimal x = showFFloat (Just 2) x ""
+
+strToCarona :: String -> Carona
+strToCarona str =
+  let parts = splitOn "," (filter (\c -> c /= '\r' && c /= '\\') str) in
+  -- traceShow parts $
+  case parts of
+    [c, h, d, o, dest, m, ps, v, am, aps] ->
+      Carona
+        { cid = read c,
+          hora = read h,
+          date = read d,
+          origem = o,
+          destino = dest,
+          motorista = m,
+          passageiros = splitOn ";" ps,
+          valor = read v,
+          avaliacaoMotorista = read am,
+          avaliacoesPassageiros = map read (splitOn ";" aps)
+        }
+    _ -> error "Invalid input format for Carona string"
+
+
+-- csv header: cid,hora,date,origem,destino,motorista,passageiros,valor,avaliacaoMotorista,avaliacoesPassageiros
+
+-- caronaToList :: Carona -> [String]
+-- toList carona =
+--   [ show (cid carona),
+--     show (hora carona),
+--     show (date carona),
+--     origem carona,
+--     destino carona,
+--     motorista carona,
+--     intercalate ";" (passageiros carona),
+--     show (valor carona),
+--     show (avaliacaoMotorista carona),
+--     intercalate ";" (map show (avaliacoesPassageiros carona))
+--   ]
