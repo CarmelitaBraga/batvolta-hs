7a47e1ce2532385131b904d0f41b40388bf24df4
CarmelitaBraga <carmelitabraga8@gmail.com>
2024-03-25 23:49:13 -0300

    craft: add skeleton for CaronaController

---
+++ Src/Controller/ControllerCarona.hs
@@ -0,0 +1,39 @@
+module Src.Controller.ControllerCarona where
+
+    import Src.Logic.CaronaLogic
+
+criarCaronaMotorista::String->(Maybe Carona)
+criarCaronaMotorista = 
+
+solicitarCaronaPassageiro::
+solicitarCaronaPassageiro = 
+
+responderSolicitacaoCarona::
+responderSolicitacaoCarona = 
+
+deletarCaronaMotorista::
+deletarCaronaMotorista =
+
+cancelarCaronaPassageiro::
+cancelarCaronaPassageiro = 
+
+atualizarStatusCarona::
+atualizarStatusCarona =
+
+mostrarCaronasPorDestino::
+mostrarCaronasPorDestino =
+
+mostrarCaronasPassageiro::
+mostrarCaronasPassageiro =
+
+mostrarCaronasMotorista::
+mostrarCaronasMotorista =
+
+mostrarCaronaPorId::
+mostrarCaronaPorId =
+
+avaliarMotoristaCarona::
+avaliarMotoristaCarona =
+
+avaliarPassageiroCarona::
+avaliarPassageiroCarona =
